463d403f554d9f43b3e5b3ef526fd80df9c0529e
zhangmin <zhangmin@baofeng.com>
2018-01-04 17:15:05 +0800

    update

--- teardown.sh
+++ teardown.sh
@@ -12,7 +12,7 @@ kubectl delete -f mysql/mysql-deployment.yaml -n $NAMESPACE
 # cdnctlmgr
 kubectl delete configmap configmap-cdnctlmgr-conf -n $NAMESPACE
 kubectl delete configmap configmap-cdnctlmgr-db -n $NAMESPACE
-kubectl delete -f CdnCtlMgrCenter/cdnctlmgr.yaml -n $NAMESPACE
+kubectl delete -f cdnctlmgr/cdnctlmgr.yaml -n $NAMESPACE
 
 # cdnmaster
 kubectl delete configmap configmap-cdnmaster-conf -n $NAMESPACE
